3775268a65ae6ef6c75cdbd03d4734644f296d69
Michael Marte <informarte@freenet.de>
2018-02-24 15:57:54 +0100

    Switched to feature scaling

--- compare-solvers.py
+++ compare-solvers.py
@@ -1,24 +1,20 @@
 #! /usr/bin/python3
 
-# This script compares the performance of a given set of solvers in a given challenge.
+# This script evaluates the performance of a given set of solvers in a given challenge.
+#
 # For each instance, the script retrieves the objective value of the best solution
-# in order to compute, for each solver S, a penalty in terms of how much worse the
-# solution of S was in comparison to the best solution.
-
-# For example, say we are considering an instance of a minimization problem,
-# the best solution's objective value was 100, and the solver S produced a solution
-# of quality 105. Then S is given a penalty of (105 - 100) / 100 = 0.05 (5%). 
-# In case the best solution has quality 0, the following approach applies:
-# The penalty is computed on a scale between 0 and 1 where 0 means the solution is one
-# of the best and 1 means the solution is one of the worst.
-
+# in order to compute, for each solver, a penalty between 0 and 1 (using feature
+# scaling) where 0 means the solution is one of the best and 1 means it is one of the
+# worst. (In case there is no solution, the penalty is 1.)
+#
 # In the end the script prints, for each solver, the number of instances it failed on,
-# and the penalties in terms of their mean, standard deviation, median, and mode.
-
+# and the penalties in terms of their mean, standard deviation, and median.
+#
 # The database is expected to reside in the working directory under the name results.db.
 
 import argparse
 import json
+import numpy
 import sqlite3
 import statistics
 import sys
@@ -28,19 +24,21 @@ def compareSolvers(cursor, challenge, solvers, verbose):
     if not jobs:
         print('There are no results for challenge {}'.format(challenge), file = sys.stderr)
         return {}
+    allSolvers = list(map(lambda result: result[0], cursor.execute('SELECT DISTINCT result.solver from result WHERE result.challenge = ?', (challenge,))));
     results = {}
     penalties = {}
     failures = {}
-    for solver in solvers:
+    for solver in allSolvers:
         results[solver] = list(cursor.execute('SELECT solved, quality FROM result WHERE challenge = ? AND solver = ? ORDER BY problem, instance', (challenge, solver)))
         if len(results[solver]) != len(jobs):
             print('Expected {} results for solver {}, but found {}'.format(len(jobs), solver, len(results[solver])), file = sys.stderr)
             return {}
+    for solver in solvers:
         penalties[solver] = []
         failures[solver] = 0
     for i in range(0, len(jobs)):
         (problem, kind, instance) = jobs[i]
-        qualities = list(map(lambda result: result[1], filter(lambda result: result[0], [results[solver][i] for solver in solvers])))
+        qualities = list(map(lambda result: result[1], filter(lambda result: result[0], [results[solver][i] for solver in allSolvers])))
         (low, high) = (None, None) if not qualities else (min(qualities), max(qualities))
         if verbose:
             print('-' * 80)
@@ -51,14 +49,15 @@ def compareSolvers(cursor, challenge, solvers, verbose):
                 if high == low:
                     penalty = 0
                 elif kind == 'MIN':
-                    penalty = (quality - low) / abs(low) if low != 0 else (quality - low) / (high - low)
+                    penalty = (quality - low) / (high - low)
                 else:
-                    penalty = (high - quality) / abs(high) if high != 0 else  (high - quality) / (high - low)
-                penalties[solver] += [penalty]
+                    penalty = 1 - ((quality - low) / (high - low))
                 if verbose:
                     print(solver, quality, penalty)
             else:
                 failures[solver] += 1
+                penalty = 1
+            penalties[solver] += [penalty]
     return {solver: {'failures': failures[solver], 'penalties': penalties[solver]} for solver in solvers}
 
 def postprocessResult(result):
@@ -70,7 +69,7 @@ def postprocessResult(result):
         'penalty-mean': statistics.mean(penalties),
         'penalty-pstdev': statistics.pstdev(penalties),
         'penalty-median': statistics.median(penalties),
-        'penalty-mode': statistics.mode(penalties)
+        'penalty-histogram': numpy.histogram(penalties, 10, (0, 1))[0].tolist()
     }
 
 def main():
