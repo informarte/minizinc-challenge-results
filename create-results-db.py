a4ed117652e35bff592b1bd3a24fca2b46fc8bf0
Michael Marte <informarte@freenet.de>
2018-03-07 17:36:20 +0100

    Replace ' ' by None

--- create-results-db.py
+++ create-results-db.py
@@ -13,7 +13,7 @@ from itertools import repeat
 def createDb(cursor):
     cursor.execute('CREATE TABLE IF NOT EXISTS problem (name TEXT PRIMARY KEY ON CONFLICT IGNORE, kind TEXT NOT NULL CONSTRAINT problem_kind_constraint CHECK (kind IN ("MIN", "MAX", "SAT"))) WITHOUT ROWID')
     cursor.execute('CREATE TABLE IF NOT EXISTS solver (name TEXT PRIMARY KEY ON CONFLICT IGNORE, fd INT NOT NULL CONSTRAINT solver_fd_constraint CHECK (fd in (0, 1)), free INT NOT NULL CONSTRAINT solver_free_constraint CHECK (free in (0, 1)), par INT NOT NULL CONSTRAINT solver_par_constraint CHECK (par in (0, 1)), open INT NOT NULL CONSTRAINT solver_open_constraint CHECK (open in (0, 1))) WITHOUT ROWID')
-    cursor.execute('CREATE TABLE IF NOT EXISTS result (challenge INT NOT NULL, solver TEXT REFERENCES solver NOT NULL, problem TEXT REFERENCES problem NOT NULL, instance TEXT NOT NULL, solved INT NOT NULL CONSTRAINT result_solved_constraint CHECK (solved in (0, 1)), complete INT NOT NULL CONSTRAINT result_complete_constraint CHECK (complete in (0, 1)), time INT NOT NULL, quality INT, CONSTRAINT result_unique_constraint UNIQUE (challenge, solver, problem, instance) ON CONFLICT IGNORE)')
+    cursor.execute('CREATE TABLE IF NOT EXISTS result (challenge INT NOT NULL, solver TEXT REFERENCES solver NOT NULL, problem TEXT REFERENCES problem NOT NULL, instance TEXT NOT NULL, solved INT NOT NULL CONSTRAINT result_solved_constraint CHECK (solved in (0, 1)), complete INT NOT NULL CONSTRAINT result_complete_constraint CHECK (complete in (0, 1)), time INT, quality INT, CONSTRAINT result_unique_constraint UNIQUE (challenge, solver, problem, instance) ON CONFLICT IGNORE)')
     cursor.execute('CREATE INDEX IF NOT EXISTS solver_index ON solver(name)')
     cursor.execute('CREATE INDEX IF NOT EXISTS result_challenge_index ON result(challenge)')
     cursor.execute('CREATE INDEX IF NOT EXISTS result_solver_index ON result(solver)')
@@ -63,10 +63,10 @@ def importResults(file, cursor):
                  repeat(solver, len(jobs)),
                  map(lambda job: job[0], jobs), # problem
                  map(lambda job: job[1], jobs), # instance
-                 map(lambda state: stateDict[state][0], states), # solved
-                 map(lambda state: stateDict[state][1], states), # complete
-                 times,
-                 objectives))
+                 map(lambda state: int(stateDict[state][0]), states), # solved
+                 map(lambda state: int(stateDict[state][1]), states), # complete
+                 map(lambda time: None if time == ' ' else int(time), times),
+                 map(lambda objective: None if objective == ' ' else int(objective), objectives)))
 
 def main():
     parser = argparse.ArgumentParser(description = 'Puts MiniZinc challenge results into database')
